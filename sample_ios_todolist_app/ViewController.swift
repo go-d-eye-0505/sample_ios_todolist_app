ea4c87c356f4fc2741b544d2b4491e089f92000f
go-d-eye-0505 <go.d.eye.0505@gmail.com>
2019-05-04 13:35:43 +0900

    コメント削除&変数名変更

--- sample_ios_todolist_app/ViewController.swift
+++ sample_ios_todolist_app/ViewController.swift
@@ -10,12 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    // テーブルの行数を返却
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoList.count
     }
 
-    // テーブルの行ごとのセルを返却
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell", for: indexPath)
         let todoTitle = todoList[indexPath.row]
@@ -32,29 +30,20 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     }
 
     @IBAction func tapAddButton(_ sender: Any) {
-        // アラートダイアログを生成
         let alertController = UIAlertController(title: "ToDo追加", message: "ToDoを入力してください", preferredStyle: UIAlertController.Style.alert)
-
-        // テキストエリアを追加
         alertController.addTextField(configurationHandler: nil)
 
-        // OKボタンを追加
         let okAction = UIAlertAction(title: "OK", style: UIAlertAction.Style.default) { (action: UIAlertAction) in
-            // OKボタンがタップされたときの処理
             if let textField = alertController.textFields?.first {
                 self.todoList.insert(textField.text!, at: 0)
                 self.tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: UITableView.RowAnimation.right)
             }
         }
-        // OKボタンがタップされたときの処理
         alertController.addAction(okAction)
 
-        // CANCELボタンがタップされたときの処理
-        let cancelButton = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
-        // CANCELボタンを追加
-        alertController.addAction(cancelButton)
+        let cancelAction = UIAlertAction(title: "CANCEL", style: UIAlertAction.Style.cancel, handler: nil)
+        alertController.addAction(cancelAction)
 
-        // アラートダイアログを表示
         present(alertController, animated: true, completion: nil)
     }
 
